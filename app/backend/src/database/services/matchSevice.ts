5bea2124c51afa81ff83ec2fdb275c6f9200d023
HenriqueSerres <henriqueserres@gmail.com>
2022-07-12 17:15:44 -0300

    feat: validations same teams and inexistent team

--- app/backend/src/database/services/matchSevice.ts
+++ app/backend/src/database/services/matchSevice.ts
@@ -1,6 +1,10 @@
 import { IMatch } from '../interfaces/matchInterface';
 import { IMatchModel } from '../protocols/matchProtocol';
 import Teams from '../models/team';
+import HandleError from '../utils/handleError';
+import TeamRepository from '../repository/teamRepository';
+
+const Team = new TeamRepository();
 
 export default class MatchService {
   constructor(private model: IMatchModel) {
@@ -17,7 +21,17 @@ export default class MatchService {
     return allMatches;
   }
 
-  async createMatches(body:object): Promise<IMatch> {
+  async createMatches(body:IMatch): Promise<IMatch> {
+    const home = await Team.findOne(body.homeTeam);
+    const away = await Team.findOne(body.awayTeam);
+
+    if (!home || !away) {
+      throw new HandleError(404, 'There is no team with such id!');
+    }
+
+    if (body.homeTeam === body.awayTeam) {
+      throw new HandleError(401, 'It is not possible to create a match with two equal teams');
+    }
     const match = await this.model.create({ ...body, inProgress: true });
 
     return match;
